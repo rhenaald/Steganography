51549a6d50a7875810f876ff497c5a3ae7a6d4c1
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-04-20 01:43:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# main.py
-
 import streamlit as st
 
-# ⬇️ Harus di baris pertama, sebelum import lokal atau Streamlit lain dipanggil
 st.set_page_config(page_title="EchoHide", layout="wide", page_icon="🎧")
 
 from ui import run_app
